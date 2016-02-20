9eb2ecab39f167f4e9199ad44d87ac7685d7d2cb
Lily Houghton <lily.anachrome@gmail.com>
2016-02-20 01:48:14 -0500

    remove unused protocol directory

--- protocol/src/drp.capnp
+++
@@ -1,7 +0,0 @@
-@0xceb71d1333e035f1;
-
-struct Message {
-    source @0 :Text;
-    dest @1 :Text;
-    body @2 :Text;
-}
--- protocol/target/debug/protocol.d
+++
@@ -1,6 +0,0 @@
-/home/lily/pogroms/porjects/drp/protocol/target/debug/protocol: src/main.rs src/drp_capnp.rs
-
-/home/lily/pogroms/porjects/drp/protocol/target/debug/protocol.d: src/main.rs src/drp_capnp.rs
-
-src/main.rs:
-src/drp_capnp.rs:
