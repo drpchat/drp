f49e0488a9471514df88585274d82ba769ce220d
Lily Houghton <lily.anachrome@gmail.com>
2016-02-19 21:03:13 -0500

    capnproto shit test

---
+++ protocol/target/debug/protocol.d
@@ -0,0 +1,6 @@
+/home/lily/pogroms/porjects/drp/protocol/target/debug/protocol: src/main.rs src/drp_capnp.rs
+
+/home/lily/pogroms/porjects/drp/protocol/target/debug/protocol.d: src/main.rs src/drp_capnp.rs
+
+src/main.rs:
+src/drp_capnp.rs:
